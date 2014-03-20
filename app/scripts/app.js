1981fe095af64f8e4447e113b103360c059c120a
Matt Hwang <mhwang206@gmail.com>
2014-03-20 11:48:24 -0700

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+/* global app:true */
+
+var app = angular.module('ngRedditApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'firebase'
+]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/posts/:postId', {
+        templateUrl: 'views/showpost.html',
+        controller: 'PostViewCtrl'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+.constant('FIREBASE_URL', 'https://ngreddit.firebaseio.com/');
